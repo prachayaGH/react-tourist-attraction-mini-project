99564149fe742f9279ad4e7ea843b98c1ed71e3c
unknown <Prachaya>
2025-03-28 10:01:22 +0700

    feat: install and configure Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return <div className="text-blue-400">Hello</div>;
 }
 
 export default App;
